c7e25e07ddf0dc60c4d27001b905bfc79a288c11
Harunobu Ishii <harunobuishii547@gmail.com>
2022-10-12 16:55:44 -0400

    email interface added

--- packages/server/src/email/email.service.ts
+++ packages/server/src/email/email.service.ts
@@ -2,25 +2,29 @@ import { Injectable } from '@nestjs/common';
 import { SqsMessageHandler, SqsConsumerEventHandler } from '@ssut/nestjs-sqs';
 import * as ses from 'node-ses';
 
+interface EmailMessage {
+  from: string;
+  to: string;
+  subject: string;
+  message: string;
+  replyTo?: string;
+  cc?: string[];
+  bcc?: string[];
+}
+
 @Injectable()
 export class EmailService {
   private readonly client = ses.createClient({} as any);
-
+  
   @SqsMessageHandler(/** name: */ 'notification queue', /** batch: */ false)
   public async handleMessage(message: AWS.SQS.Message) {
-    const msg = JSON.parse(message.Body);
-
+    const msg: EmailMessage = JSON.parse(message.Body) as EmailMessage;
+    console.log(msg)
     // Give SES the details and let it construct the message for you.
     this.client.sendEmail(
-      {
-        from: 'test@email.sail.codes',
-        subject: 'Greetings',
-        message: 'Hello',
-        altText: 'plain text',
-        to: 'hishii@bu.edu',
-      },
+        msg,
       function (err, data, res) {
-        console.log(res);
+        // console.log(res);
       },
     );
   }
