641b5f1d2cc822d3dd9d1bc8d27078c4d3d126e3
Snafkin547 <62607343+Snafkin547@users.noreply.github.com>
2022-10-12 11:00:17 -0400

    Send receive works (#1)
    * Simple send and receive messages
    
    * Simple send and receive messages
    
    * SQS and SES running
    
    * gitignore changed
    
    * node module deleted
    
    * ignore changes
    
    * cleaned up code
    
    * cleaned up code
    
    * cleaned up code
    
    * cleaned up code
    
    * fix: package lock
    
    * fix: formatting
    
    Co-authored-by: Snafkin547 <harunobuishii@gmail.com>
    Co-authored-by: Greg Frasco <frascog17@gmail.com>

---
+++ packages/server/src/email/email.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { EmailService } from './email.service';
+import { SqsModule } from '@ssut/nestjs-sqs';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    SqsModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        return {
+          consumers: [{ name: 'notification queue', queueUrl: configService.get('SQS_QUEUE_URL') }],
+        };
+      },
+    }),
+  ],
+  providers: [EmailService],
+})
+export class EmailModule {}
