e978f03d159d26b782364173f34260c953e7b123
Harunobu Ishii <harunobuishii547@gmail.com>
2022-10-13 12:00:13 -0400

    email interface added

--- packages/server/src/email/email.service.ts
+++ packages/server/src/email/email.service.ts
@@ -10,8 +10,7 @@ export class EmailService {
   public async handleMessage(message: AWS.SQS.Message) {
     const msg: EmailMessage = JSON.parse(message.Body) as EmailMessage;
     // Give SES the details and let it construct the message for you.
-    this.client.sendEmail(msg, function (err, data, res) {
-    });
+    this.client.sendEmail(msg, function (err, data, res) {});
   }
 
   @SqsConsumerEventHandler(/** name: */ 'notification queue', /** eventName: */ 'processing_error')
