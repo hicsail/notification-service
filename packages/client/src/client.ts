89ae989bbfbb64121357952c3e8f2b75229d1234
Harunobu Ishii <harunobuishii547@gmail.com>
2022-10-17 15:30:17 -0400

    Developing client lib

--- packages/client/src/client.ts
+++ packages/client/src/client.ts
@@ -11,13 +11,8 @@ export class NotificationClient {
   });
 
   public async sendmessage(msg: object, delaySeconds = 5) {
-    try {
       const formattedEmail = await validate(msg);
       const id = uuidv4();
-      await this.producer.send([{ id, delaySeconds, body: JSON.stringify(formattedEmail) }]);
-    } catch (error) {
-      console.log(error);
-      console.log('The email fomrat is invalid. Please review the above error message and resend again');
-    }
+      return this.producer.send([{ id, delaySeconds, body: JSON.stringify(formattedEmail) }]);
   }
 }
