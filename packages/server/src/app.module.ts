72428b85223daba2027c51cbe3bbe78437115826
wenhwang97 <wenhan.wang97@gmail.com>
2022-10-18 09:31:46 -0400

    Added health module

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { HealthModule } from './health/health.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EmailModule } from './email/email.module';
 import { ConfigModule } from '@nestjs/config';
 // SESModule,
 @Module({
-  imports: [EmailModule, ConfigModule.forRoot({ isGlobal: true })],
+  imports: [EmailModule, HealthModule, ConfigModule.forRoot({ isGlobal: true })],
   controllers: [AppController],
   providers: [AppService],
 })
