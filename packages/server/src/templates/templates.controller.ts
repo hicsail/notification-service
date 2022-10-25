78862830ebde59df30fff58218f1110c0d2f3a09
Harunobu Ishii <harunobuishii547@gmail.com>
2022-10-25 11:52:04 -0400

    1st commit template

--- packages/server/src/templates/templates.controller.ts
+++ packages/server/src/templates/templates.controller.ts
@@ -1,10 +1,9 @@
-import React from 'react';
 import { Controller, Get } from '@nestjs/common';
 import { TemplatesService } from './templates.service';
 
 @Controller('templates')
 export class TemplatesController {
-  constructor(private readonly templateService: TemplatesService) {}
+  constructor(private readonly templateService: TemplatesService) { }
 
   @Get('/')
   getTemplate(): any {
--- packages/server/src/templates/templates.service.tsx
+++ packages/server/src/templates/templates.service.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Injectable } from '@nestjs/common';
 import ReactDOMServer from 'react-dom/server';
 import CssBaseline from '@mui/material/CssBaseline';
