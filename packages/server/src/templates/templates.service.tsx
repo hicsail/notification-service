4780abd3cdf30be75988394c96b05af67a4210d9
Harunobu Ishii <harunobuishii547@gmail.com>
2022-10-26 15:58:52 -0400

    template updated

--- packages/server/src/templates/templates.service.tsx
+++ packages/server/src/templates/templates.service.tsx
@@ -3,11 +3,11 @@ import { passwordReset, sample } from './email_templates'
 
 @Injectable()
 export class TemplatesService {
+
   /// Step by step guideline
   /// 1) Create a new subdirectory in email_templates (e.g., passwordReset) and locate all relevant files in it
   /// 2) Export in index.js in email_templates
   /// 3) Import the newly created function above and add in the following object/dictionary
-
   templateStyles = {
     "sample": sample(),
     "passwordReset": passwordReset()
