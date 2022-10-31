77b64ca92e15f1853f3c62eaee8367666433998d
Harunobu Ishii <harunobuishii547@gmail.com>
2022-10-31 15:22:01 -0400

    template updated

--- packages/server/src/templates/projects/auth/passwordReset.tsx
+++ packages/server/src/templates/projects/auth/passwordReset.tsx
@@ -1,74 +1,44 @@
-import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme();
 
 export function passwordReset() {
 
-    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
-            first: data.get('password'),
-            second: data.get('password2'),
-        });
-    };
-
     return (
         ReactDOMServer.renderToString(
-            <ThemeProvider theme={theme}>
-                <Container component="main" maxWidth="xs">
-                    <CssBaseline />
-                    <Box
-                        sx={{
-                            marginTop: 8,
-                            display: 'flex',
-                            flexDirection: 'column',
-                            alignItems: 'center',
-                        }}
-                    >
-                        <Typography component="h1" variant="h5">
-                            Change Password
-                        </Typography>
-                        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                name="password"
-                                label="Password"
-                                type="password"
-                                id="password"
-                                InputLabelProps={{ shrink: true }}
-                            />
-                            <TextField
-                                margin="normal"
-                                required
-                                fullWidth
-                                name="password2"
-                                label="Confirm Password"
-                                type="password"
-                                id="password2"
-                                InputLabelProps={{ shrink: true }}
-                            />
-                            <Button
-                                type="submit"
-                                fullWidth
-                                variant="contained"
-                                sx={{ mt: 3, mb: 2 }}
-                            >
-                                Change Password
-                            </Button>
-                        </Box>
+            <Container component="main" maxWidth="xs">
+                <Box
+                    sx={{
+                        marginTop: 10,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    <Typography component="h1" variant="h5">
+                        Reset Password
+                    </Typography>
+                    <br></br>
+                    <br></br>
+                    <div>You may reset your password following the link below</div>
+                    <Box component="form" noValidate sx={{ mt: 1 }}>
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                            href='/pwreset'
+                        >
+                            Reset Password
+                        </Button>
                     </Box>
-                </Container>
-            </ThemeProvider>
-        ))
+                    <br></br>
+                    <div>If you did not request a PW reset, please ignore this email </div>
+                </Box>
+            </Container>))
 }
