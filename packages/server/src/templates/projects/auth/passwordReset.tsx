347e470125d1368442269dd376ec131ea217b872
Harunobu Ishii <harunobuishii547@gmail.com>
2022-10-31 15:23:12 -0400

    template updated

--- packages/server/src/templates/projects/auth/passwordReset.tsx
+++ packages/server/src/templates/projects/auth/passwordReset.tsx
@@ -10,35 +10,34 @@ const theme = createTheme();
 export function passwordReset() {
 
     return (
-        ReactDOMServer.renderToString(
-            <Container component="main" maxWidth="xs">
-                <Box
-                    sx={{
-                        marginTop: 10,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                    <Typography component="h1" variant="h5">
+        <Container component="main" maxWidth="xs">
+            <Box
+                sx={{
+                    marginTop: 10,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Typography component="h1" variant="h5">
+                    Reset Password
+                </Typography>
+                <br></br>
+                <br></br>
+                <div>You may reset your password following the link below</div>
+                <Box component="form" noValidate sx={{ mt: 1 }}>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        sx={{ mt: 3, mb: 2 }}
+                        href='/pwreset'
+                    >
                         Reset Password
-                    </Typography>
-                    <br></br>
-                    <br></br>
-                    <div>You may reset your password following the link below</div>
-                    <Box component="form" noValidate sx={{ mt: 1 }}>
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                            href='/pwreset'
-                        >
-                            Reset Password
-                        </Button>
-                    </Box>
-                    <br></br>
-                    <div>If you did not request a PW reset, please ignore this email </div>
+                    </Button>
                 </Box>
-            </Container>))
+                <br></br>
+                <div>If you did not request a PW reset, please ignore this email </div>
+            </Box>
+        </Container>)
 }
