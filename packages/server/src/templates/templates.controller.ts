aec28a1f395f980ce04a6374cb8240a2a45b103a
Harunobu Ishii <harunobuishii547@gmail.com>
2022-11-03 15:43:02 -0400

    Dynamic template import implemented

--- packages/server/src/templates/templates.controller.ts
+++ packages/server/src/templates/templates.controller.ts
@@ -3,7 +3,7 @@ import { TemplatesService } from './templates.service';
 
 @Controller('templates')
 export class TemplatesController {
-  constructor(private readonly templateService: TemplatesService) { }
+  constructor(private readonly templateService: TemplatesService) {}
 
   @Post('/preview/:templateName')
   postTemplate(@Param('templateName') templateName: string, @Body() body: any): any {
