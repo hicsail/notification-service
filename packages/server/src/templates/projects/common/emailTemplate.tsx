a126c0825bb06ddd36114ff7cf1b258ce99ed6eb
cbolles <collinbolles@gmail.com>
2022-12-14 16:54:50 -0500

    Basic card wrapper with different backgrounds

--- packages/server/src/templates/projects/common/emailTemplate.tsx
+++ packages/server/src/templates/projects/common/emailTemplate.tsx
@@ -1,19 +1,22 @@
-import { FC } from 'react';
+import React, { FC } from 'react';
 import Container from '@mui/material/Container';
 import CardHeader from '@mui/material/CardHeader';
 import Card from '@mui/material/Card';
+import { useTheme } from '@mui/material/styles';
 
 export interface EmailTemplateProps {
   title: string;
   subheader: string;
-  children: any;
   footer?: string;
+  children?: React.ReactNode;
 }
 
-export const EmailTemplate: FC<EmailTemplateProps> = (props: EmailTemplateProps) => {
+export const EmailTemplate: FC<EmailTemplateProps> = (props) => {
+  const theme = useTheme();
+
   return(
-    <Container component="main" maxWidth="xs">
-      <Card>
+    <Container component="main" sx={{ bgcolor: theme.palette.background.default }} maxWidth="md">
+      <Card sx={{ bgcolor: theme.palette.background.paper }}>
         <CardHeader title = {props.title} subheader = {props.subheader} />
 
         {props.children}
