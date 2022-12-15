d8c6ee365e9493f2d7d5381104d57f33bda608af
cbolles <collinbolles@gmail.com>
2022-12-15 12:07:42 -0500

    Get box to center

--- packages/server/src/templates/projects/common/emailTemplate.tsx
+++ packages/server/src/templates/projects/common/emailTemplate.tsx
@@ -1,5 +1,5 @@
 import React, { FC } from 'react';
-import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
 import CardHeader from '@mui/material/CardHeader';
 import Card from '@mui/material/Card';
 import Divider from '@mui/material/Divider';
@@ -17,18 +17,20 @@ export const EmailTemplate: FC<EmailTemplateProps> = (props) => {
   const theme = useTheme();
 
   return(
-    <Container component="main" sx={{ bgcolor: theme.palette.background.default }} maxWidth="md">
-      <Card sx={{ bgcolor: theme.palette.background.paper, padding: theme.spacing(2) }}>
-        <CardHeader title = {props.title} subheader = {props.subheader} />
+    <Box sx={{bgcolor: theme.palette.background.default, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
+      <Box sx={{maxWidth: 'md', margin: 'auto' }}>
+        <Card sx={{ bgcolor: theme.palette.background.paper, padding: theme.spacing(2) }}>
+          <CardHeader title = {props.title} subheader = {props.subheader} />
+          <Divider />
+
+          {props.children}
+        </Card>
+
         <Divider />
-
-        {props.children}
-      </Card>
-
-      <Divider />
-      <div style={{background: theme.palette.background.paper, padding: theme.spacing(2) }}>
-        {props.footer}
-      </div>
-    </Container>
+        <div style={{background: theme.palette.background.paper, padding: theme.spacing(2) }}>
+          {props.footer}
+        </div>
+      </Box>
+    </Box>
   )
 }
