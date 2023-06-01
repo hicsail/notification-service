23a737ed76d3affd26d7f0c56fedc28c96416100
Harunobu Ishii <harunobuishii547@gmail.com>
2023-06-01 11:24:48 -0400

    telemetry baseline

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -3,9 +3,10 @@ import { HealthModule } from './health/health.module';
 import { EmailModule } from './email/email.module';
 import { ConfigModule } from '@nestjs/config';
 import { TemplatesModule } from './templates/templates.module';
+import { TelemetryModule } from './telemetry/telemetry.module';
 
 // SESModule,
 @Module({
-  imports: [EmailModule, HealthModule, TemplatesModule, ConfigModule.forRoot({ isGlobal: true })]
+  imports: [EmailModule, HealthModule, TemplatesModule, ConfigModule.forRoot({ isGlobal: true }), TelemetryModule]
 })
 export class AppModule {}
